d0b8c17eeb1766c7b8ac64fcaf9fe1376ef630ae
VimukthiWaththegama <vwaththegama@gmail.com>
2024-08-19 12:19:03 +0530

    add

--- department-service/department-service/src/main/java/com/vimukthibw/department_service/repository/DepartmentRepo.java
+++ department-service/department-service/src/main/java/com/vimukthibw/department_service/repository/DepartmentRepo.java
@@ -1,4 +1,33 @@
 package com.vimukthibw.department_service.repository;
 
-public interface DepartmentRepo {
+import com.vimukthibw.department_service.model.DepartmentModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+
+public class DepartmentRepo {
+
+    private List<DepartmentModel> departments = new ArrayList<>();
+
+    public DepartmentModel addDepartment( DepartmentModel departmentModel )
+    {
+        departments.add( departmentModel );
+        return departmentModel;
+    }
+
+    public DepartmentModel findById( Long id )
+    {
+        return departments.stream().
+                           filter( departmentModel -> departmentModel.getDepId().equals( id ) ).
+                           findFirst().orElseThrow();
+    }
+    public List<DepartmentModel> findAll(){
+        return departments;
+    }
+
+    public void deleteDepartment(Long id){
+        departments.remove( findById( id ) );
+    }
 }
